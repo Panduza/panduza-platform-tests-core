7b2e997be476c342cae929691108e56790165cf3
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-06-30 06:57:23 +0100

    up

--- scripts/tests_registers.py
+++ scripts/tests_registers.py
@@ -15,12 +15,14 @@ for topic, info in interfaces.items():
 reg_map = Registers(client=client, topic="pza/default/map/map")
 # reg_map = Registers(client=client, topic="pza/default/test_map/map")
 
+reg_map.write(4, [10])
+
 
 print("writereg")
 reg_map.write(0, [42])
 reg_map.write(5, [1,2,3])
 
 print("read")
-reg_map.read(0, 2)
-
+val = reg_map.read(0, 2, 1000)
+print(val)
 
